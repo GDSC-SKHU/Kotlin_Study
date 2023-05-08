673a7e54586ff8a4c22d627d6d03d3cf69ca6bb4
kikingki <mongbu54@gmail.com>
2023-05-09 01:51:07 +0900

    Add: 코틀린 프로젝트 생성(#6)

---
+++ 이지선/2주차/Kotlin-First-Project/src/main/kotlin/com/gdsc/kotlinfirstproject/KotlinFirstProjectApplication.kt
@@ -0,0 +1,11 @@
+package com.gdsc.kotlinfirstproject
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinFirstProjectApplication
+
+fun main(args: Array<String>) {
+	runApplication<KotlinFirstProjectApplication>(*args)
+}
