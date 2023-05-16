4f0b8295f70e10b46b94101aff8e5493bae6625e
kikingki <mongbu54@gmail.com>
2023-05-17 00:57:19 +0900

    Add: 프로젝트 생성(#11)

---
+++ 이지선/3주차/Kotlin-Post-Project/src/main/kotlin/com/gdsc/kotlinpostproject/KotlinPostProjectApplication.kt
@@ -0,0 +1,11 @@
+package com.gdsc.kotlinpostproject
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinPostProjectApplication
+
+fun main(args: Array<String>) {
+	runApplication<KotlinPostProjectApplication>(*args)
+}
