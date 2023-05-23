0b98348a9ee16747f53ee5030e2d5ecf5cd3faa0
kikingki <mongbu54@gmail.com>
2023-05-24 01:00:05 +0900

    Add: 테스트 코드 작성(#16)

---
+++ 이지선/4주차/tdd-study/src/main/java/com/gdsc/tddstudy/component/ICalculator.java
@@ -0,0 +1,7 @@
+package com.gdsc.tddstudy.component;
+
+public interface ICalculator {
+    void init();
+    int sum(int x, int y);
+    int minus(int x, int y);
+}
---
+++ 이지선/4주차/tdd-study/src/main/java/com/gdsc/tddstudy/dto/Req.java
@@ -0,0 +1,14 @@
+package com.gdsc.tddstudy.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Req {
+
+    private int x;
+    private int y;
+}
